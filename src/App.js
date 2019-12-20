5d17ad44e1db401e7e1f55511070957f8057ddcd
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2019-12-19 23:34:23 -0500

    Made loads of changes and went back to using formik. form is complete. need backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Forms from './Forms';
+// import Userlist from './Userlist';
 import './App.css';
+import styled from 'styled-components';
+//import logo from './Minimallogo.png';
+const Style = styled.div`
+display: flex;
+margin: 0 auto;
+width: 58%;
+padding: 25%;
+background #18181E ;
+color: #DEC79B;
+`
+// const Img = styled.img `
+// width: 2%;
+// `
 
 function App() {
+  const [members, setMembers] = useState([{
+    name: '',
+    email:'',
+    password: ''
+  }]);
+  const addNewMember = teamMember =>{
+    setMembers([...members, teamMember]);
+  };
+
+
   return (
+    // <Img><img src={logo} alt="Logo" />;</Img>
+    <Style>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Minimal Lift</h2>
+      <Forms addNewMember={addNewMember}/>
     </div>
+    </Style>
   );
-}
+};
 
 export default App;
---
+++ src/Forms.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+const SignUp = ({ errors, touched, values }) => {
+  return (
+    <div className='form'>
+      <h4>Sign Up</h4>
+
+      <Form>
+        <Field
+          type='name'
+          name='name'
+          placeholder='Name'
+          className='input'
+        /><br/>
+        {touched.name && errors.name && <p className='error'>{errors.name}</p>}
+        <Field
+          type='email'
+          name='email'
+          placeholder='Email'
+          className='input'
+        /><br/>
+        {touched.email && errors.email && (
+          <p className='error'>{errors.email}</p>
+        )}
+        <Field
+          type='password'
+          name='password'
+          placeholder='Password'
+          className='input'
+        /><br/>
+        {touched.password && errors.password && (
+          <p className='error'>{errors.password}</p>
+        )}<br/>
+        {/* This here is an optinal thing and I'm unsure if I want to use it. */}
+        <label className='terms'>
+          <Field type='checkbox' name='term' checked={values.term} /> I agree to
+          the <a href='https://github.com/emster7013'>Terms and Conditions</a>
+        </label>
+        <button type='submit' className='submit'>
+          Submit
+        </button>
+      </Form>
+    </div>
+  )
+}
+
+const FormikApp = withFormik({
+  mapPropsToValues({ name, email, password, term }) {
+    return {
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      term: term || false
+    }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string()
+      .email()
+      .required(),
+    password: Yup.string()
+      .min(0)
+      .required()
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+      console.log('submitting', values);
+      axios
+      .post('https://reqres.in/api/users', values)
+      .then(res => {
+         console.log('success', res);
+         setStatus(res.data);
+         resetForm();
+      })
+      .catch(err => console.log('NOOOOO!!!', err.response));
+   },
+  // handleSubmit(values, { resetForm }) {
+  //   //Include an axios.get here.
+  //   console.log(values)
+  //   resetForm()
+  // }
+})
+const FormSignUp = FormikApp(SignUp)
+
+export default FormSignUp;
